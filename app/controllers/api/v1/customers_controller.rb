edc1c4f128d2a29ceebcbfd830b1d99c8c292d8e
ryu0114h <ryu0114h@gmail.com>
2021-02-22 17:19:00 +0900

    create destroy_api & update_api

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -15,6 +15,24 @@ module Api
         end
       end
 
+      def destroy
+        if customer = Customer.find_by(id: params[:id])
+          customer.destroy
+          render json: { status: 200, message: "Deleted the customer", data: customer }
+        else
+          render json: { status: 400, data: customer.errors }
+        end
+      end
+
+      def update
+        if customer = Customer.find_by(id: params[:id])
+          customer.update(customer_params)
+          render json: { status: 200, message: "update the customer", data: customer }
+        else
+          render json: { status: 400, data: customer.errors }
+        end
+      end
+
       def customer_params
         params.require(:customer).permit(:firstName, :lastName, :age, :address, :memo, :tags)
       end
