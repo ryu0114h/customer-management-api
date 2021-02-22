c9432a5c18fc39d4e31edb406eb1e06391f42612
ryu0114h <ryu0114h@gmail.com>
2021-02-22 14:45:09 +0900

    create create_API

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -5,6 +5,19 @@ module Api
         customers = Customer.all
         render json: { status: 200, message: "Loaded customers", data: customers }
       end
+
+      def create
+        customer = Customer.new(customer_params)
+        if customer.save
+          render json: { status: 200, data: customer }
+        else
+          render json: { status: 400, data: customer.errors }
+        end
+      end
+
+      def customer_params
+        params.require(:customer).permit(:firstName, :lastName, :age, :address, :memo, :tags)
+      end
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   namespace "api" do
     namespace "v1" do
       resources :customers
-      root "customers#index"
     end
   end
 end
