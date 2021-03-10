5a8f3e5bf3042aadb523af8d8080fe77a290e12c
ryu0114h <ryu0114h@gmail.com>
2021-03-10 23:50:26 +0900

    create reservations_destroy_api

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -15,6 +15,15 @@ class Api::V1::ReservationsController < ApplicationController
     end
   end
 
+  def destroy
+    if reservation = Reservation.find_by(id: params[:id])
+      reservation.destroy
+      render json: { status: 200, message: "Deleted the reservation", data: reservation }
+    else
+      render json: { status: 400, data: reservation.errors }
+    end
+  end
+
   def reservation_params
     params.require(:reservation).permit(:id, :name, :all_day, :start_datetime, :end_datetime, :user_id, :created_at, :updated_at)
   end
