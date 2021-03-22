bdad54060f49ca6c99d3bb16d830ccfca93cecc3
ryu0114h <ryu0114h@gmail.com>
2021-03-22 11:51:06 +0900

    deviseでstaffを作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
     namespace "v1" do
       resources :reservations
       resources :customers
-      mount_devise_token_auth_for "User", at: "auth"
+      mount_devise_token_auth_for "User", at: "user_auth"
+      mount_devise_token_auth_for "Staff", at: "staff_auth"
     end
   end
 end
