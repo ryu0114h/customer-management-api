31d1afe7894630e06fcf50d0f78a8ca04de55a5f
ryu0114h <ryu0114h@gmail.com>
2021-04-03 21:24:44 +0900

    警告に対する修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  namespace "api" do
-    namespace "v1" do
+  namespace 'api' do
+    namespace 'v1' do
       resources :reservations
       resources :users
       resources :staff
 
-      mount_devise_token_auth_for "Staff",
-                                  at: "staff_auth",
+      mount_devise_token_auth_for 'Staff',
+                                  at: 'staff_auth',
                                   controllers: {
-                                    registrations: "api/v1/staff/registrations",
+                                    registrations: 'api/v1/staff/registrations'
                                   }
     end
   end
