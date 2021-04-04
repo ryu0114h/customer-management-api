9c2a6b601e4cc9c2c78c4994aa677e456b7a33f5
ryu0114h <ryu0114h@gmail.com>
2021-04-04 09:55:55 +0900

    不要なstaff変数を削除

--- app/controllers/api/v1/staff_controller.rb
+++ app/controllers/api/v1/staff_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::StaffController < ApplicationController
   def index
-    if staff = Staff.find(current_api_v1_staff.id)
-      render json: { status: 200, message: "Loaded staff", data: staff }
+    if current_api_v1_staff
+      render json: { status: 200, message: 'Loaded staff', data: current_api_v1_staff }
     else
-      render json: { status: 400, message: "Not Loaded staff" }
+      render json: { status: 400, message: 'Not Loaded staff' }
     end
   end
 end
