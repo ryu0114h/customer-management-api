227b0d659bc9f61405c505dbf654f14fb54aa9d0
ryu0114h <ryu0114h@gmail.com>
2021-04-06 02:06:58 +0900

    画像をアップロードできるように

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_04_131926) do
+ActiveRecord::Schema.define(version: 2021_04_05_152246) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 2021_04_04_131926) do
     t.string "postal_code"
     t.string "address"
     t.string "introduction_text"
+    t.string "image_url"
     t.index ["confirmation_token"], name: "index_staffs_on_confirmation_token", unique: true
     t.index ["email"], name: "index_staffs_on_email", unique: true
     t.index ["reset_password_token"], name: "index_staffs_on_reset_password_token", unique: true
