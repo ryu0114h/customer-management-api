007ff008e158aeb6b101e2194fa923524ff680d5
ryu0114h <ryu0114h@gmail.com>
2021-05-10 17:33:50 +0900

    migration

--- db/migrate/20210309195013_create_reservations.rb
+++ db/migrate/20210309195013_create_reservations.rb
@@ -1,14 +1,12 @@
 class CreateReservations < ActiveRecord::Migration[6.1]
   def change
-    def change
-      create_table :reservations do |t|
-        t.bigint "staff_id", null: false
-        t.bigint "user_id"
-        t.boolean "all_day"
-        t.datetime "start_datetime"
-        t.datetime "end_datetime"
-        t.timestamps
-      end
+    create_table :reservations do |t|
+      t.bigint 'staff_id', null: false
+      t.bigint 'user_id'
+      t.boolean 'all_day'
+      t.datetime 'start_datetime'
+      t.datetime 'end_datetime'
+      t.timestamps
     end
   end
 end
