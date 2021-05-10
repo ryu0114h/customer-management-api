01396f3b59c926a1c48de1fcf445a107d8f55680
ryu0114h <ryu0114h@gmail.com>
2021-05-10 17:07:04 +0900

    migration

--- db/migrate/20210328193513_add_column_to_users.rb
+++ db/migrate/20210328193513_add_column_to_users.rb
@@ -1,8 +1,8 @@
 class AddColumnToUsers < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :postal_code, :string
-    add_column :users, :address, :string
-    add_column :users, :introduction_text, :text
-    remove_column :users, :nickname, :string
+    # add_column :users, :postal_code, :string
+    # add_column :users, :address, :string
+    # add_column :users, :introduction_text, :text
+    # remove_column :users, :nickname, :string
   end
 end
