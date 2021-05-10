e55f9a128a5d2527e00f64cdd21daa8081359d6e
ryu0114h <ryu0114h@gmail.com>
2021-05-10 17:25:31 +0900

    migration

--- db/migrate/20210223140228_drop_table_user.rb
+++ db/migrate/20210223140228_drop_table_user.rb
@@ -1,6 +1,6 @@
 class DropTableUser < ActiveRecord::Migration[6.1]
   def change
-    drop_table :users
+    # drop_table :users
     drop_table :ar_internal_metadata
     drop_table :schema_migrations
   end
